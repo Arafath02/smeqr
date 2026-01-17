fdddae3ad992b957eebad4104a3d5c939aa5ea37
Arafath02 <arafathlafeer11@gmail.com>
2026-01-17 16:17:35 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const client = urlParams.get("client");
 
 // Fetch client JSON
-//fetch(`/clients/${client}.json`)
-fetch(`clients/${client}.json`)
+fetch(`/clients/${client}.json`)
   .then(response => {
     if (!response.ok) throw new Error("Client JSON not found");
     return response.json();
@@ -72,4 +71,5 @@ fetch(`clients/${client}.json`)
     const descContainer = document.getElementById("description-container");
     descContainer.innerText = "Client not found.";
   });
+
 
